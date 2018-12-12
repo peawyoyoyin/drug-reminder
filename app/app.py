4f6443f2d4e7429b08d296038b3cb5d66cc45cfc
Piriyapong Laopongsit <peawyoyoyin@gmail.com>
2018-12-12 15:36:56 +0700

    add /process-url

--- app/app.py
+++ app/app.py
@@ -2,8 +2,11 @@ import os
 from flask import Flask, request, jsonify
 import numpy as np
 from werkzeug.utils import secure_filename
+import shutil
 
 from datetime import datetime
+
+import requests
 
 from druginfo import parser
 from tesseract import tesseract
@@ -27,6 +30,29 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route('/')
 def greet():
     return 'greetings from drug-reminder'
+
+@app.route('/process-url', methods=['POST'])
+def process_url():
+    url = request.values['url']
+    res = requests.get(url, stream=True)
+
+    if res.status == 200:
+        filename = secure_filename(url) + datetime.now().strftime('%Y-%m-%d(%H-%M-%S)')
+        save_destination = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        with open(save_destination, 'wb') as tmp_image_file:
+            shutil.copyfileobj(res.raw, tmp_image_file)
+        del res
+
+        image_as_nparray = np.array(imageio.imread(save_destination))
+
+        # print(type(image_as_nparray), image_as_nparray.shape)
+        text_from_image = tesseract.getText(image_as_nparray)
+        drug_info = parser.getDrugInfo(text_from_image)
+
+        if not app.config['KEEP_FILES']:
+            os.remove(save_destination)
+
+        return jsonify(drug_info)
 
 @app.route('/process', methods=['POST'])
 def process_image():
